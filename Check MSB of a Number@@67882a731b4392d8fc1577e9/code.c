6d4522ba5e78dbb645ff7a42ad36ccd37d52ca0e
dhandtanmay2612 <tanmaydhand834@gmail.com>
2025-01-29 18:36:14 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <limits.h> // For INT_MAX
 
-int main(){
+int main() {
     int num;
+
+    printf("Enter a number: ");
     scanf("%d", &num);
-    if(num >= (1 << 31)){
+
+    // Check if the number is greater than or equal to 2^31 (which is the value when MSB is set)
+    if (num >= (1 << 31)) { 
         printf("Set\n");
-    }
-    else{
+    } else {
         printf("Not Set\n");
     }
+
     return 0;
 }
