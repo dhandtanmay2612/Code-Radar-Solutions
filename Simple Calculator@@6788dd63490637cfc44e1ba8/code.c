483a3fd5cbe04513eb359cc39e01e95987029006
Tanmay Dhand <tanmaydhand834@gmail.com>
2025-03-10 16:21:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,6 +6,9 @@ int main() {
 
     scanf("%d %d %c", &num1, &num2, &operator);
 
+    // Consume any leading whitespace before reading the operator
+    scanf(" %c", &operator); // Add a space before %c
+
     switch (operator) {
         case '+':
             printf("%d\n", num1 + num2);
